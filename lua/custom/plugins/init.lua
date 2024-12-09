687b124d248d16fdf51e553f2ed291fa236cbf4a
Anthony Shorter <anthony.shorter@theNBS.com>
2024-12-09 17:05:27 +0000

    Add lazygit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,19 +4,40 @@
 -- See the kickstart.nvim README for more information
 -- Lazy
 return {
-  'jackMort/ChatGPT.nvim',
-  event = 'VeryLazy',
-  config = function()
-    local chatgpt = require 'chatgpt'
-    chatgpt.setup()
-    vim.keymap.set('v', '<leader>pe', function()
-      chatgpt.edit_with_instructions()
-    end, { desc = 'ChatGPT: Edit with instructions' })
-  end,
-  dependencies = {
-    'MunifTanjim/nui.nvim',
-    'nvim-lua/plenary.nvim',
-    'folke/trouble.nvim', -- optional
-    'nvim-telescope/telescope.nvim',
+  {
+    'jackMort/ChatGPT.nvim',
+    event = 'VeryLazy',
+    config = function()
+      local chatgpt = require 'chatgpt'
+      chatgpt.setup()
+      vim.keymap.set('v', '<leader>pe', function()
+        chatgpt.edit_with_instructions()
+      end, { desc = 'ChatGPT: Edit with instructions' })
+    end,
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'folke/trouble.nvim', -- optional
+      'nvim-telescope/telescope.nvim',
+    },
+  },
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
   },
 }
